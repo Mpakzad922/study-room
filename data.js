10c25ad8425ceaf32543423fc2c47c2e6e4a6610
Mpakzad922 <stonefieldkevin7@gmail.com>
2026-01-01 21:14:38 +0330

    Update data.js

--- data.js
+++ data.js
@@ -15,7 +15,13 @@ const LOCAL_PLAYLIST = [
     { 
         id: "3", 
         title: "درس سوم: علوم (با چند ضمیمه)", 
-        file: "https://dl.example.com/video3.mp4", 
+        file: "https://s6.uupload.ir/filelink/JIGqKSwUYgM9_26b4f05e12/karbarg5_tr6h.mp4", 
         attachment: "خلاصه درس|https://site.com/pdf1.pdf , نمونه سوال|https://site.com/pdf2.pdf , پاسخنامه|https://site.com/pdf3.pdf" 
+    }
+      { 
+        id: "4", 
+        title: "ریاضی کاربرگ", 
+        file: "https://s6.uupload.ir/filelink/JIGqKSwUYgM9_26b4f05e12/karbarg5_tr6h.mp4", 
+        attachment: 
     }
 ];
