7efc40beaf356f1307829365920095e90d7d9ae5
Mpakzad922 <stonefieldkevin7@gmail.com>
2026-01-01 21:28:28 +0330

    Update data.js

--- data.js
+++ data.js
@@ -1,27 +1,15 @@
-// لیست درس‌ها برای حالت آفلاین (ضد فیلتر)
 const LOCAL_PLAYLIST = [
-    { 
-        id: "6", 
-        title: "درس اول: آشنایی با کلاس", 
-        file: "https://s6.uupload.ir/filelink/XVBYMDVSfc8L_6b49ffb4ca/karbarg5_tr6h.mp4", 
-        attachment: "" 
+    {
+        id: "101", // سعی کن آیدی‌های data.js با گوگل شیت تداخل نداشته باشد (مثلا ۳ رقمی بزار)
+        title: "درس اول: معرفی ",
+        file: "LINK_FILM_1.mp4",
+        attachment: ""
     },
-    { 
-        id: "7", 
-        title: "درس دوم: ریاضی (با یک ضمیمه)", 
-        file: "https://dl.example.com/video2.mp4", 
-        attachment: "دانلود جزوه|https://site.com/jozve.pdf" 
-    },
-    { 
-        id: "8", 
-        title: "درس سوم: علوم (با چند ضمیمه)", 
-        file: "https://s6.uupload.ir/filelink/JIGqKSwUYgM9_26b4f05e12/karbarg5_tr6h.mp4", 
+    {
+        id: "102",
+        title: "درس دوم: مرور ریاضی ",
+        file: "LINK_FILM_2.mp4",
         attachment: "خلاصه درس|https://site.com/pdf1.pdf , نمونه سوال|https://site.com/pdf2.pdf , پاسخنامه|https://site.com/pdf3.pdf" 
     }
-      { 
-        id: "9", 
-        title: "ریاضی کاربرگ", 
-        file: "https://s6.uupload.ir/filelink/JIGqKSwUYgM9_26b4f05e12/karbarg5_tr6h.mp4", 
-        attachment: 
-    }
 ];
+
